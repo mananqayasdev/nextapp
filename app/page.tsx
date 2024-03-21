723ede4eda7c14593015978375d0515acb563adb
mananqayasdev <mrmuhammadqayas@gmail.com>
2024-03-21 02:00:08 -0400

    do add all
    most of the stuff
    yes
    ok
    
    yes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
 import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
